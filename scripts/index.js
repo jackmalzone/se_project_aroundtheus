157ec920553c85531eeaf819edd26c45b68e6d4b
JAK <56848280+jackmalzone@users.noreply.github.com>
2024-04-20 13:40:15 -0700

    feat/refactor add new card functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 const initialCards = [
   {
-    name: "Yosemite Valley",
+    place: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
     alt: "Sunset view of Yosemite Valley with El Capitan visible, river flowing in the foreground surrounded by dense pine trees.",
   },
   {
-    name: "Lake Louise",
+    place: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
     alt: "Calm waters of Lake Louise reflecting the surrounding mountains under a soft blue sky.",
   },
   {
-    name: "Bald Mountains",
+    place: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
     alt: "Sunburst peaking over the silhouette of the Bald Mountains at sunrise, casting a warm glow across the hazy, layered ridges.",
   },
   {
-    name: "Latemar",
+    place: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
     alt: "Starry night sky over the jagged peaks of Latemar mountain range with remnants of snow highlighting its rugged terrain.",
   },
   {
-    name: "Vanoise National Park",
+    place: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
     alt: "Twilight hues casting a soft glow on the peaks of Vanoise National Park, reflected in the still waters of a mountain lake.",
   },
   {
-    name: "Lago di Braies",
+    place: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     alt: "Wooden boats moored at a dock on Lago di Braies with towering Dolomite mountains reflecting in the crystal-clear lake water.",
   },
@@ -38,6 +38,9 @@ const initialCards = [
 // Wrappers
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileAddModal = document.querySelector("#profile-add-modal");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const profileAddForm = profileAddModal.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
 
 // Buttons
 const profileEditButton = document.querySelector("#profile-edit-button");
@@ -59,9 +62,9 @@ const profileInputName = document.querySelector("#modal-input-name");
 const profileInputDescription = document.querySelector(
   "#modal-input-description"
 );
-const profileEditForm = profileEditModal.querySelector(".modal__form");
-const profileAddForm = profileAddModal.querySelector(".modal__form");
-const cardList = document.querySelector(".cards__list");
+const profileInputPlace = profileAddForm.querySelector("#modal-input-place");
+const profileInputLink = profileAddForm.querySelector("#modal-input-link");
+
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 // const modalTemplate = document.querySelector("#modal-template").content;
@@ -71,18 +74,31 @@ const cardTemplate =
 function closeModal(modal) {
   modal.classList.remove("modal_opened");
 }
-
 function openModal(modal) {
   modal.classList.add("modal_opened");
 }
 
+function renderCard(cardData, wrapper) {
+  const cardElement = getCardElement(cardData);
+  wrapper.prepend(cardElement);
+}
+
 // EVENT HANDLERS
 
-function handleFormSubmit(evt) {
+function handleEditFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = profileInputName.value;
   profileDescription.textContent = profileInputDescription.value;
-  closeModal();
+  closeModal(profileEditModal);
+  // console.log("Form submitted, no refresh!");
+}
+
+function handleAddFormSubmit(evt) {
+  evt.preventDefault();
+  const place = profileInputPlace.value;
+  const link = profileInputLink.value;
+  renderCard({ place, link }, cardList);
+  closeModal(profileAddModal);
   // console.log("Form submitted, no refresh!");
 }
 
@@ -92,7 +108,7 @@ function getCardElement(cardData) {
   const cardCaption = cardElement.querySelector(".card__caption");
   cardImage.src = cardData.link;
   cardImage.alt = cardData.alt;
-  cardCaption.textContent = cardData.name;
+  cardCaption.textContent = cardData.place;
   // console.log(cardData);
   return cardElement;
 }
@@ -113,20 +129,15 @@ profileEditButton.addEventListener("click", () => {
   profileInputDescription.value = profileDescription.textContent;
   openModal(profileEditModal);
 });
-
-// profileEditButton.addEventListener("click", () => openModal(profileEditModal);
 profileEditButtonClose.addEventListener("click", () =>
   closeModal(profileEditModal)
 );
-profileEditForm.addEventListener("submit", handleFormSubmit);
+profileEditForm.addEventListener("submit", handleEditFormSubmit);
 
 profileAddButton.addEventListener("click", () => openModal(profileAddModal));
 profileAddButtonClose.addEventListener("click", () =>
   closeModal(profileAddModal)
 );
-profileAddForm;
+profileAddForm.addEventListener("submit", handleAddFormSubmit);
 
-initialCards.forEach((cardData) => {
-  const cardElement = getCardElement(cardData);
-  cardList.prepend(cardElement);
-});
+initialCards.forEach((cardData) => renderCard(cardData, cardList));
