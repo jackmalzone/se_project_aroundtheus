34429e4449207a86584a10f766ef84987893cf4d
JAK <56848280+jackmalzone@users.noreply.github.com>
2024-06-04 22:58:50 -0700

    refactor _toggleButtonState

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -44,8 +44,7 @@ export default class FormValidator {
 
   _toggleButtonState() {
     if (this._hasInvalidInput()) {
-      this._submitButton.classList.add(this._settings.inactiveButtonClass);
-      this._submitButton.disabled = true;
+      this.disableButton();
     } else {
       this._submitButton.classList.remove(this._settings.inactiveButtonClass);
       this._submitButton.disabled = false;
